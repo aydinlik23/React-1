8487e163c03978fe0ee19b32adb932a2ed179521
aydinlik23 <50822442+aydinlik23@users.noreply.github.com>
2019-12-04 14:48:52 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Cover from './Cover'
+
+function App(){
+    return(
+        <div>
+            <Navbar />
+            <Cover />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
